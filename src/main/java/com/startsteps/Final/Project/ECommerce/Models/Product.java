fc8842abf83a6c72096f841dded7718f1e983a89
hcs <hannahemcs@gmail.com>
2024-01-08 15:40:32 +0100

    Create Product model

---
+++ src/main/java/com/startsteps/Final/Project/ECommerce/Models/Product.java
@@ -0,0 +1,77 @@
+package com.startsteps.Final.Project.ECommerce.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer productId;
+    private String productName;
+    private Integer inStock;
+    private double unitCost;
+    private double unitPrice;
+    private ProductCategory productCategory;
+
+    public Product() {
+    }
+
+    public Product(String productName, Integer inStock, double unitCost, double unitPrice, ProductCategory productCategory) {
+        this.productName = productName;
+        this.inStock = inStock;
+        this.unitCost = unitCost;
+        this.unitPrice = unitPrice;
+        this.productCategory = productCategory;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Integer getInStock() {
+        return inStock;
+    }
+
+    public void setInStock(Integer inStock) {
+        this.inStock = inStock;
+    }
+
+    public double getUnitCost() {
+        return unitCost;
+    }
+
+    public void setUnitCost(double unitCost) {
+        this.unitCost = unitCost;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public ProductCategory getProductCategory() {
+        return productCategory;
+    }
+
+    public void setProductCategory(ProductCategory productCategory) {
+        this.productCategory = productCategory;
+    }
+}
---
+++ src/main/java/com/startsteps/Final/Project/ECommerce/Models/ProductCategory.java
@@ -0,0 +1,5 @@
+package com.startsteps.Final.Project.ECommerce.Models;
+
+public enum ProductCategory {
+    CLOTHING, ACCESSORIES, SHOES, MISC
+}
