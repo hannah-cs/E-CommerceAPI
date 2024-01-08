4f04ded0a9ff40c0251cdd73ca6a5b80ffb746ab
hcs <hannahemcs@gmail.com>
2024-01-08 15:25:41 +0100

    Initial commit

---
+++ src/main/java/com/startsteps/Final/Project/ECommerce/FinalProjectECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.startsteps.Final.Project.ECommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/startsteps/Final/Project/ECommerce/FinalProjectECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.startsteps.Final.Project.ECommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
