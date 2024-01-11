6de2c94b8face678315d0008e43f620a9309640e
hcs <hannahemcs@gmail.com>
2024-01-11 11:31:56 +0100

    Implement swagger

--- src/main/java/com/startsteps/Final/Project/ECommerce/FinalProjectECommerceApplication.java
+++ src/main/java/com/startsteps/Final/Project/ECommerce/FinalProjectECommerceApplication.java
@@ -2,8 +2,10 @@ package com.startsteps.Final.Project.ECommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class FinalProjectECommerceApplication {
 
 	public static void main(String[] args) {
