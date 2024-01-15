0dd411f5c813a851ba62451ca5af6bc27aec5d79
hcs <hannahemcs@gmail.com>
2024-01-15 15:22:21 +0100

    Accidentally undid commit to remove swagger annotation, removed again

--- src/main/java/com/startsteps/Final/Project/ECommerce/FinalProjectECommerceApplication.java
+++ src/main/java/com/startsteps/Final/Project/ECommerce/FinalProjectECommerceApplication.java
@@ -2,12 +2,10 @@ package com.startsteps.Final.Project.ECommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
-@EnableSwagger2
 public class FinalProjectECommerceApplication {
 	static final Logger LOG = LoggerFactory.getLogger(FinalProjectECommerceApplication.class);
 	public static void main(String[] args) {
