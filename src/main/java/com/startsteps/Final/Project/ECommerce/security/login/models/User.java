53192dd21a47d79560bfa79b88860c52b7db53f9
hcs <hannahemcs@gmail.com>
2024-01-17 11:59:48 +0100

    Create user and roles classes for jwt security

---
+++ src/main/java/com/startsteps/Final/Project/ECommerce/security/login/models/User.java
@@ -0,0 +1,93 @@
+package com.startsteps.Final.Project.ECommerce.security.login.models;
+
+import com.startsteps.Final.Project.ECommerce.Models.User.UserRole;
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "userId"),
+                @UniqueConstraint(columnNames = "email")
+        })
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer userId;
+    private String email;
+    private String name;
+    private String password;
+    List<String> allOrders;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(  name = "user_roles",
+            joinColumns = @JoinColumn(name = "userId"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+    @Enumerated(EnumType.STRING)
+    private ERole userRole;
+
+
+    public User() {
+    }
+
+    public User(String email, String name, String password, ERole eRole) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        Role role = new Role(eRole);
+        this.roles.add(role);
+        this.userRole = eRole;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<String> getAllOrders() {
+        return allOrders;
+    }
+
+    public void setAllOrders(List<String> allOrders) {
+        this.allOrders = allOrders;
+    }
+
+    public UserRole getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(UserRole userRole) {
+        this.userRole = userRole;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
