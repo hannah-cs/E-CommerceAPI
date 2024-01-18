e6606140e7adc41e13d51249b2aba1a58b2052ff
hcs <hannahemcs@gmail.com>
2024-01-18 14:49:35 +0100

    Bug fixes so that app runs successfully

--- src/main/java/com/startsteps/Final/Project/ECommerce/security/login/WebSecurityConfig.java
+++ src/main/java/com/startsteps/Final/Project/ECommerce/security/login/WebSecurityConfig.java
@@ -4,22 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.startsteps.Final.Project.ECommerce.security.login.jwt.AuthEntryPointJwt;
+import com.startsteps.Final.Project.ECommerce.security.login.jwt.AuthTokenFilter;
+import com.startsteps.Final.Project.ECommerce.security.login.services.UserDetailsServiceImpl;
+
 @Configuration
 @EnableMethodSecurity
-(securedEnabled = true,
-jsr250Enabled = true,
-prePostEnabled = true)
 public class WebSecurityConfig {
+
     @Autowired
     UserDetailsServiceImpl userDetailsService;
 
@@ -30,15 +34,16 @@ public class WebSecurityConfig {
     public AuthTokenFilter authenticationJwtTokenFilter() {
         return new AuthTokenFilter();
     }
+
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-
-        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setUserDetailsService((UserDetailsService) userDetailsService);
         authProvider.setPasswordEncoder(passwordEncoder());
 
         return authProvider;
     }
+
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
@@ -59,8 +64,9 @@ public class WebSecurityConfig {
                                 .requestMatchers("/api/test/**").permitAll()
                                 .anyRequest().authenticated()
                 );
-        http.authenticationProvider(authenticationProvider());
 
+        http.headers(headers -> headers.frameOptions(frameOption -> frameOption.sameOrigin()));
+        http.authenticationProvider(authenticationProvider());
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
