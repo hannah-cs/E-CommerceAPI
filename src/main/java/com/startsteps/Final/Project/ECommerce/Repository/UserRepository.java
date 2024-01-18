ea628f09f6d2923afdef4a0a3275ee2c6b155609
hcs <hannahemcs@gmail.com>
2024-01-18 14:11:00 +0100

    Delete old classes, will rebuild later to align with new user class

--- src/main/java/com/startsteps/Final/Project/ECommerce/Repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.startsteps.Final.Project.ECommerce.Repository;
-
-import com.startsteps.Final.Project.ECommerce.Models.User.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
