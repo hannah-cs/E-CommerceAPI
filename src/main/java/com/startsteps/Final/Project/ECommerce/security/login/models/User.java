397160e7843d0b0d46ec86c41bfa011820a2188f
hcs <hannahemcs@gmail.com>
2024-01-19 15:48:37 +0100

    Edit generation type to correctly auto increment

--- src/main/java/com/startsteps/Final/Project/ECommerce/security/login/models/User.java
+++ src/main/java/com/startsteps/Final/Project/ECommerce/security/login/models/User.java
@@ -14,7 +14,7 @@ import java.util.Set;
         })
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer userId;
     private String username;
     private String email;
