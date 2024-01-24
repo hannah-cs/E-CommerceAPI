1fbc3569cfe80ab801f31f9f2861f819bae43153
hcs <hannahemcs@gmail.com>
2024-01-24 16:58:24 +0100

    SFP-59 : createProduct endpoint

--- src/main/java/com/startsteps/Final/Project/ECommerce/ProductManagement/controllers/ProductController.java
+++ src/main/java/com/startsteps/Final/Project/ECommerce/ProductManagement/controllers/ProductController.java
@@ -1,6 +1,7 @@
 package com.startsteps.Final.Project.ECommerce.ProductManagement.controllers;
 
 
+import com.startsteps.Final.Project.ECommerce.ProductManagement.models.Product;
 import com.startsteps.Final.Project.ECommerce.ProductManagement.services.ProductService;
 import com.startsteps.Final.Project.ECommerce.security.login.WebSecurityConfig;
 import com.startsteps.Final.Project.ECommerce.security.login.jwt.JwtUtils;
@@ -12,9 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/product")
@@ -38,6 +37,24 @@ public class ProductController {
             User user = userRepository.findByUsername(username).orElse(null);
             if (user != null){
                 return ResponseEntity.ok().body(new MessageResponse(productService.getAllProducts().toString()));
+            } else {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                        .body(new MessageResponse("You must be an admin to perform this action."));
+            }
+        }
+        return ResponseEntity.badRequest().body(new MessageResponse("You must be logged in to access this feature."));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createProduct(HttpServletRequest request, @RequestBody Product newProduct){
+        String jwt = jwtUtils.getJwtFromCookies(request);
+        if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
+            String username = jwtUtils.getUserNameFromJwtToken(jwt);
+            System.out.println(username);
+            User user = userRepository.findByUsername(username).orElse(null);
+            if (user != null){
+                productService.createProduct(newProduct);
+                return ResponseEntity.ok().body(new MessageResponse("Product added to database."));
             } else {
                 return ResponseEntity.status(HttpStatus.FORBIDDEN)
                         .body(new MessageResponse("You must be an admin to perform this action."));
