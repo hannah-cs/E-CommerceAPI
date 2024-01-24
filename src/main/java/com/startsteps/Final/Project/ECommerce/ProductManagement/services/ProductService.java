66012057b86bd6c9a47ad1d4f4b6dfbd8c684e35
hcs <hannahemcs@gmail.com>
2024-01-24 16:40:12 +0100

    SFP-59 : add get all products method to service class

--- src/main/java/com/startsteps/Final/Project/ECommerce/ProductManagement/services/ProductService.java
+++ src/main/java/com/startsteps/Final/Project/ECommerce/ProductManagement/services/ProductService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,6 +20,10 @@ public class ProductService {
     public Product loadProductById(Integer productId) throws ProductNotFoundException {
         Optional<Product> optionalProduct = productRepository.findById(productId);
         return optionalProduct.orElseThrow(() -> new ProductNotFoundException("Product not found with id: " + productId));
+    }
+
+    public List<Product> getAllProducts(){
+        return productRepository.findAll();
     }
 
 }
