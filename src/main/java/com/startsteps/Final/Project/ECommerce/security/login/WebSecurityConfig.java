d2a844beb4821a7ee3171353d55834e4579ec96c
hcs <hannahemcs@gmail.com>
2024-01-26 10:40:45 +0100

    Fix jwt to use cookie header

--- src/main/java/com/startsteps/Final/Project/ECommerce/security/login/WebSecurityConfig.java
+++ src/main/java/com/startsteps/Final/Project/ECommerce/security/login/WebSecurityConfig.java
@@ -71,8 +71,7 @@ public class WebSecurityConfig {
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> {
-                            auth.requestMatchers("/api/auth/**").permitAll()
-                                    .requestMatchers("/api/test/**").permitAll();
+                            auth.requestMatchers("/api/auth/**").permitAll();
                             auth.anyRequest().authenticated();
                         }
                 );
