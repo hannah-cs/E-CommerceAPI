83b23e649d869a3a2e711abcb43e4f666c00d118
hcs <hannahemcs@gmail.com>
2024-01-30 17:47:35 +0100

    SFP-26 order processing

--- src/main/java/com/startsteps/Final/Project/ECommerce/OrderManagement/models/OrderStatus.java
+++ src/main/java/com/startsteps/Final/Project/ECommerce/OrderManagement/models/OrderStatus.java
@@ -1,5 +1,5 @@
 package com.startsteps.Final.Project.ECommerce.OrderManagement.models;
 
 public enum OrderStatus {
-    IN_CART, PENDING, PROCESSING, SHIPPED, COMPLETED, CANCELLED, RETURNED;
+    IN_CART, PROCESSING, SHIPPED, DELIVERED, COMPLETED, CANCELLED, RETURNED;
 }
