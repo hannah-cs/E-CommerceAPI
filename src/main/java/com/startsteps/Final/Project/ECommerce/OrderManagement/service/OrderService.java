eec8a0b53bfd219f38cae0841db95321c21552c1
hcs <hannahemcs@gmail.com>
2024-02-02 12:04:57 +0100

    SFP-24 add cart methods

--- src/main/java/com/startsteps/Final/Project/ECommerce/OrderManagement/service/OrderService.java
+++ src/main/java/com/startsteps/Final/Project/ECommerce/OrderManagement/service/OrderService.java
@@ -6,6 +6,7 @@ import com.startsteps.Final.Project.ECommerce.OrderManagement.models.OrderStatus
 import com.startsteps.Final.Project.ECommerce.OrderManagement.models.ProductsOrders;
 import com.startsteps.Final.Project.ECommerce.OrderManagement.repository.OrderRepository;
 import com.startsteps.Final.Project.ECommerce.ProductManagement.models.Product;
+import com.startsteps.Final.Project.ECommerce.ProductManagement.repository.ProductRepository;
 import com.startsteps.Final.Project.ECommerce.security.login.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -21,6 +22,8 @@ import java.util.Optional;
 public class OrderService {
     @Autowired
     OrderRepository orderRepository;
+    @Autowired
+    ProductRepository productRepository;
     @Transactional
     public Order loadOrderById(Integer orderId) throws OrderNotFoundException {
         Optional<Order> optionalOrder = orderRepository.findById(orderId);
@@ -67,184 +70,97 @@ public class OrderService {
     }
 
     @Transactional
-    public void addToCart(int userId, Product product, int quantity) {
+    public void addToCart(int userId, int productId, int quantity) {
         Order existingOrder = orderRepository.findByUserIdAndOrderStatus(userId, OrderStatus.IN_CART)
                 .stream()
                 .findFirst()
-                .orElse(null);
+                .orElseGet(() -> {
+                    Order newOrder = new Order(userId, OrderStatus.IN_CART);
+                    orderRepository.save(newOrder);
+                    return newOrder;
+                });
 
-        if (existingOrder != null) {
-            ProductsOrders productsOrders = new ProductsOrders(existingOrder, product, quantity);
-            existingOrder.addProductOrder(productsOrders);
-            orderRepository.save(existingOrder);
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new IllegalArgumentException("Product not found"));
+
+        ProductsOrders productsOrders = new ProductsOrders(existingOrder, product, quantity);
+        existingOrder.addProductOrder(productsOrders);
+        orderRepository.save(existingOrder);
+    }
+
+    public void cancelOrder(int orderId){
+        Order order = orderRepository.findById(orderId).orElse(null);
+        if (order == null){
+            System.out.println("No order found with id "+orderId);
         } else {
-            Order order = new Order(userId, OrderStatus.IN_CART);
+            if (order.getOrderStatus() == OrderStatus.CANCELLED){
+                System.out.println("Order already cancelled.");
+            } else if (order.getOrderStatus() == OrderStatus.SHIPPED){
+                System.out.println("Order has already been shipped. Cannot be cancelled.");
+            } else {
+                order.setOrderStatus(OrderStatus.CANCELLED);
+                order.setOrderDate(LocalDateTime.now());
+                System.out.println("Order cancelled successfully.");
+            }
+        }
+    }
+
+    public void returnOrder(int orderId){
+        Order order = orderRepository.findById(orderId).orElse(null);
+        if (order == null){
+            System.out.println("No order found with id "+orderId);
+        } else {
+            if (order.getOrderStatus() == OrderStatus.SHIPPED){
+                order.setOrderStatus(OrderStatus.RETURNED);
+                System.out.println("Return process started successfully.");
+            } else if (order.getOrderStatus() == OrderStatus.COMPLETED){
+                System.out.println("Error: return window exceeded");
+            } else {
+                System.out.println("Error: return could not be processed. Double check order status.");
+            }
+        }
+    }
+
+    // for admins
+    public void shipOrder(int orderId){
+        Order order = orderRepository.findById(orderId).orElse(null);
+        if (order == null){
+            System.out.println("No order found with id "+orderId);
+        } else {
+            if (order.getOrderStatus() == OrderStatus.PROCESSING){
+                order.setOrderStatus(OrderStatus.SHIPPED);
+                order.setShipDate(LocalDateTime.now());
+                System.out.println("Order shipped successfully.");
+            } else {
+                System.out.println("Error: order cancelled or already shipped");
+            }
+        }
+    }
+
+    // auto mark orders as completed if not returned within 30 days of shipping
+    @Scheduled(cron = "0 0 0 * * ?")
+    public void completeShippedOrders() {
+        List<Order> deliveredOrders = orderRepository.findByOrderStatus(OrderStatus.SHIPPED);
+        for (Order order : deliveredOrders) {
+            LocalDateTime shipDate = order.getShipDate();
+            LocalDateTime currentDate = LocalDateTime.now();
+            long daysDifference = ChronoUnit.DAYS.between(shipDate, currentDate);
+            int daysThreshold = 30;
+            if (daysDifference >= daysThreshold) {
+                order.setOrderStatus(OrderStatus.COMPLETED);
+                orderRepository.save(order);
+                System.out.println("Order " + order.getOrderId() + " automatically marked as completed. Return window closed.");
+            }
+        }
+    }
+
+    public void checkoutOrder(int orderId) {
+        Order order = orderRepository.findById(orderId).orElse(null);
+
+        if (order != null && order.getOrderStatus() == OrderStatus.IN_CART) {
+            order.setOrderStatus(OrderStatus.PROCESSING);
+            order.setOrderDate(LocalDateTime.now());
             orderRepository.save(order);
-            ProductsOrders productsOrders = new ProductsOrders(order, product, quantity);
-            order.addProductOrder(productsOrders);
-        }
-    }
-
-
-    public void checkoutOrder(int orderId){
-        Order order = orderRepository.findById(orderId).orElse(null);
-        if (order == null){
-            System.out.println("No order found with id "+orderId);
-        } else {
-            if (order.getOrderStatus() != OrderStatus.IN_CART){
-                System.out.println("Order already processed");
-            } else {
-                order.setOrderStatus(OrderStatus.PROCESSING);
-                order.setOrderDate(LocalDateTime.now());
-                System.out.println("Order placed successfully.");
-            }
-        }
-    }
-
-    public void cancelOrder(int orderId){
-        Order order = orderRepository.findById(orderId).orElse(null);
-        if (order == null){
-            System.out.println("No order found with id "+orderId);
-        } else {
-            if (order.getOrderStatus() == OrderStatus.CANCELLED){
-                System.out.println("Order already cancelled.");
-            } else if (order.getOrderStatus() == OrderStatus.SHIPPED){
-                System.out.println("Order has already been shipped. Cannot be cancelled.");
-            } else {
-                order.setOrderStatus(OrderStatus.CANCELLED);
-                order.setOrderDate(LocalDateTime.now());
-                System.out.println("Order cancelled successfully.");
-            }
-        }
-    }
-
-    public void returnOrder(int orderId){
-        Order order = orderRepository.findById(orderId).orElse(null);
-        if (order == null){
-            System.out.println("No order found with id "+orderId);
-        } else {
-            if (order.getOrderStatus() == OrderStatus.SHIPPED){
-                order.setOrderStatus(OrderStatus.RETURNED);
-                System.out.println("Return process started successfully.");
-            } else if (order.getOrderStatus() == OrderStatus.COMPLETED){
-                System.out.println("Error: return window exceeded");
-            } else {
-                System.out.println("Error: return could not be processed. Double check order status.");
-            }
-        }
-    }
-
-    // for admins
-    public void shipOrder(int orderId){
-        Order order = orderRepository.findById(orderId).orElse(null);
-        if (order == null){
-            System.out.println("No order found with id "+orderId);
-        } else {
-            if (order.getOrderStatus() == OrderStatus.PROCESSING){
-                order.setOrderStatus(OrderStatus.SHIPPED);
-                order.setShipDate(LocalDateTime.now());
-                System.out.println("Order shipped successfully.");
-            } else {
-                System.out.println("Error: order cancelled or already shipped");
-            }
-        }
-    }
-
-    // auto mark orders as completed if not returned within 30 days of shipping
-    @Scheduled(cron = "0 0 0 * * ?")
-    public void completeShippedOrders() {
-        List<Order> deliveredOrders = orderRepository.findByOrderStatus(OrderStatus.SHIPPED);
-        for (Order order : deliveredOrders) {
-            LocalDateTime shipDate = order.getShipDate();
-            LocalDateTime currentDate = LocalDateTime.now();
-            long daysDifference = ChronoUnit.DAYS.between(shipDate, currentDate);
-            int daysThreshold = 30;
-            if (daysDifference >= daysThreshold) {
-                order.setOrderStatus(OrderStatus.COMPLETED);
-                orderRepository.save(order);
-                System.out.println("Order " + order.getOrderId() + " automatically marked as completed. Return window closed.");
-            }
-        }
-    }
-
-    public void checkoutOrder(int orderId){
-        Order order = orderRepository.findById(orderId).orElse(null);
-        if (order == null){
-            System.out.println("No order found with id "+orderId);
-        } else {
-            if (order.getOrderStatus() != OrderStatus.IN_CART){
-                System.out.println("Order already processed");
-            } else {
-                order.setOrderStatus(OrderStatus.PROCESSING);
-                order.setOrderDate(LocalDateTime.now());
-                System.out.println("Order placed successfully.");
-            }
-        }
-    }
-
-    public void cancelOrder(int orderId){
-        Order order = orderRepository.findById(orderId).orElse(null);
-        if (order == null){
-            System.out.println("No order found with id "+orderId);
-        } else {
-            if (order.getOrderStatus() == OrderStatus.CANCELLED){
-                System.out.println("Order already cancelled.");
-            } else if (order.getOrderStatus() == OrderStatus.SHIPPED){
-                System.out.println("Order has already been shipped. Cannot be cancelled.");
-            } else {
-                order.setOrderStatus(OrderStatus.CANCELLED);
-                order.setOrderDate(LocalDateTime.now());
-                System.out.println("Order cancelled successfully.");
-            }
-        }
-    }
-
-    public void returnOrder(int orderId){
-        Order order = orderRepository.findById(orderId).orElse(null);
-        if (order == null){
-            System.out.println("No order found with id "+orderId);
-        } else {
-            if (order.getOrderStatus() == OrderStatus.SHIPPED){
-                order.setOrderStatus(OrderStatus.RETURNED);
-                System.out.println("Return process started successfully.");
-            } else if (order.getOrderStatus() == OrderStatus.COMPLETED){
-                System.out.println("Error: return window exceeded");
-            } else {
-                System.out.println("Error: return could not be processed. Double check order status.");
-            }
-        }
-    }
-
-    // for admins
-    public void shipOrder(int orderId){
-        Order order = orderRepository.findById(orderId).orElse(null);
-        if (order == null){
-            System.out.println("No order found with id "+orderId);
-        } else {
-            if (order.getOrderStatus() == OrderStatus.PROCESSING){
-                order.setOrderStatus(OrderStatus.SHIPPED);
-                order.setShipDate(LocalDateTime.now());
-                System.out.println("Order shipped successfully.");
-            } else {
-                System.out.println("Error: order cancelled or already shipped");
-            }
-        }
-    }
-
-    // auto mark orders as completed if not returned within 30 days of shipping
-    @Scheduled(cron = "0 0 0 * * ?")
-    public void completeShippedOrders() {
-        List<Order> deliveredOrders = orderRepository.findByOrderStatus(OrderStatus.SHIPPED);
-        for (Order order : deliveredOrders) {
-            LocalDateTime shipDate = order.getShipDate();
-            LocalDateTime currentDate = LocalDateTime.now();
-            long daysDifference = ChronoUnit.DAYS.between(shipDate, currentDate);
-            int daysThreshold = 30;
-            if (daysDifference >= daysThreshold) {
-                order.setOrderStatus(OrderStatus.COMPLETED);
-                orderRepository.save(order);
-                System.out.println("Order " + order.getOrderId() + " automatically marked as completed. Return window closed.");
-            }
         }
     }
 }
